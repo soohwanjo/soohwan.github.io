1edff544946e2a6779136f0f46fe4196a35a64a7
soohwanjo <jsh14828@gmail.com>
2023-07-29 22:35:07 +0900

    chg to thumbnails

--- src/assets/photos.ts
+++ src/assets/photos.ts
@@ -1,9 +1,9 @@
 const imgList = require.context('./gallery', false, /^(assets).*\.webp$/)
-const images: String[] = imgList.keys()
+const photoFiles: String[] = imgList.keys()
 
 // const breakpoints: number[] = [2160, 1080, 640, 256, 128];
 
-export const photos = images.map((file_nm: String) => {
+export const photos = photoFiles.map((file_nm: String) => {
     var tmp = file_nm.split('.')[0].split('_');
     var w: number = parseInt(tmp[tmp.length - 2]);
     var h: number = parseInt(tmp[tmp.length - 1]);
@@ -20,5 +20,20 @@ export const photos = images.map((file_nm: String) => {
         //         height: breakpointHeight
         //     }
         // })
+    }
+});
+
+const thmbList = require.context('./thumbnail', false, /^(assets).*\.webp$/)
+const thumbFiles: String[] = thmbList.keys()
+
+export const thumbs = thumbFiles.map((file_nm: String) => {
+    var tmp = file_nm.split('.')[0].split('_');
+    var w: number = parseInt(tmp[tmp.length - 2]);
+    var h: number = parseInt(tmp[tmp.length - 1]);
+    
+    return {
+        src: require('../' + file_nm),
+        width: w,
+        height: h
     }
 });
--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -6,7 +6,7 @@ import { mobileWidth } from '../configs';
 import useWindowSize from '../utils/useWindowSize'
 import iconArrow from '../assets/images/icon_down_arrow.png';
 
-import { photos } from "../assets/photos";
+import { photos, thumbs } from "../assets/photos";
 import PhotoAlbum from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
@@ -51,7 +51,7 @@ const Photo = () => {
 
     const targetRowHeight: number = (size.width < mobileWidth? size.width*0.8/3: size.width*0.8/5);
     const spacing: number = (size.width < mobileWidth? 5: 10);
-    const initCount: number = (size.width < mobileWidth? 12: 15);
+    const initCount: number = (size.width < mobileWidth? 15: 20);
 
     const onClickShow = () => setIsShow(!isShow);
     const onHover = () => {
@@ -61,30 +61,14 @@ const Photo = () => {
     return (
         <Section id="photo" >
             <Title main="사 진 첩" sub="G A L L E R Y" />
-            {!isShow
-                ? <>
-                    <PhotoAlbum
-                        layout="rows"
-                        spacing={spacing}
-                        photos={photos.slice(0,initCount)}
-                        targetRowHeight={targetRowHeight}
-                        onClick={({ index }) => setIndex(index)}
-                    />
-                    <div style={{marginTop: spacing}}>
-                        <DivMore onClick={onClickShow} onMouseOver={onHover}>
-                            <TextMediumDodum>사진 더보기</TextMediumDodum><br/>
-                            <img src={iconArrow} alt="down-arrow" />
-                        </DivMore>
-                    </div>
-                </>                    
-                : <PhotoAlbum
-                    layout="rows"
-                    spacing={spacing}
-                    photos={photos}
-                    targetRowHeight={targetRowHeight}
-                    onClick={({ index }) => setIndex(index)}
-                />
-            }
+            <PhotoAlbum
+                layout="rows"
+                spacing={spacing}
+                photos={thumbs}
+                targetRowHeight={targetRowHeight}
+                onClick={({ index }) => setIndex(index)}
+            />
+            <br/>
             <Lightbox
                 index={index}
                 slides={photos}
